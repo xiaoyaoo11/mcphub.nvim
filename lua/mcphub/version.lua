84e1f8f984372a9e4ac96a115e529bf5502cb479
ravitemer <ravitemer@gmail.com>
2025-03-06 08:25:42 +0000

    chore(release): bump version to 2.0.1

--- lua/mcphub/version.lua
+++ lua/mcphub/version.lua
@@ -1,5 +1,5 @@
 return {
-    PLUGIN_VERSION = "2.0.0", -- Current plugin version
+    PLUGIN_VERSION = "2.0.1", -- Current plugin version
     REQUIRED_NODE_VERSION = { -- Required mcp-hub version
         major = 1,
         minor = 4,
