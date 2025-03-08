eb7583bc521b0f62ba9b25819332069596c2beaf
ravitemer <ravitemer@gmail.com>
2025-03-08 06:04:12 +0000

    release v2.2.0

--- lua/mcphub/version.lua
+++ lua/mcphub/version.lua
@@ -1,9 +1,9 @@
 return {
-    PLUGIN_VERSION = "2.1.2", -- Current plugin version
+    PLUGIN_VERSION = "2.2.0", -- Current plugin version
     REQUIRED_NODE_VERSION = { -- Required mcp-hub version
         major = 1,
         minor = 5,
         patch = 0,
-        string = "1.5.0"
-    }
+        string = "1.5.0",
+    },
 }
